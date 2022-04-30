c4b4bc3f4cf902fb83f33fde967e35e815316f11
GeorgeSouzaFreire <ge10l@hotmail.com>
2022-04-29 23:03:44 -0300

    Commit - 120

--- routes/pedidoRotas.js
+++ routes/pedidoRotas.js
@@ -73,36 +73,31 @@ router.post('/PostPedido/', async (req, res) => {
                 })
             } else {
 
-                
-
-                pedido.produto.forEach(quantidadePOST => {
+                for (let k = 0; k < pedido.produto.length; k++) {
 
                     let soma = 0;
 
-                    soma = quantidadePOST.quantidade
+                    soma = pedido.produto[k].quantidade
 
-                    pedidoFindOne.produto.forEach(quantidadeBD => {
+                    for (let j = 0; j < pedidoFindOne.produto.length; j++) {
 
-                        if (quantidadePOST.idProduto.String == quantidadeBD.idProduto.String) {
+                        if (pedido.produto[k].idProduto == pedidoFindOne.produto[j].idProduto) {
 
-                            soma += quantidadeBD.quantidade;
+                            soma += pedidoFindOne.produto[j].quantidade;
 
-                            quantidadeBD.quantidade = soma
+                            pedidoFindOne.produto[j].quantidade = soma
 
                             console.log('Soma das quantidades', soma)
-                        } else {
-                            console.log(':( Não são iguais ------------> ', quantidadeBD.quantidade)
                         }
+                    }
+                }
 
-                    });
-                });
-
-                const pedidoOneAndUpdate = await Pedido.findOneAndUpdate({ _id: pedidoFindOne._id }, pedidoFindOne)
+                const pedidoUpdate = await Pedido.findOneAndUpdate({ _id: pedidoFindOne._id }, pedidoFindOne, { new: true })
 
                 res.status(201).json({
                     success: true,
                     message: "Pedido atualizado com sucesso!",
-                    data: pedidoOneAndUpdate,
+                    data: pedidoUpdate,
                 })
 
             }
@@ -158,8 +153,138 @@ router.get('/GetPedidoApp', async (req, res) => {
             error: error
         })
     }
+})
 
+// Get Pedido App
+router.patch('/AtualizaQuantidadePedido', async (req, res) => {
 
+    const pedidoId = req.query.IdPedido
+    const tipo = req.query.Tipo
+
+    const {
+        guid,
+        idEmpresa,
+        idUsuario,
+        nomeUsuario,
+        endereco,
+        produto,
+        formaPagamento,
+        statusPedido,
+        idStatusPedido,
+        ativo,
+        dataCriacao,
+        dataAtualizacao,
+    } = req.body
+
+    const pedido = {
+        guid,
+        idEmpresa,
+        idUsuario,
+        nomeUsuario,
+        endereco,
+        produto,
+        formaPagamento,
+        statusPedido,
+        idStatusPedido,
+        ativo,
+        dataCriacao,
+        dataAtualizacao,
+    }
+
+    try {
+
+        if (tipo == 0) {
+
+            const pedidoFindOne = await Pedido.findOne({ _id: pedidoId })
+
+            for (let k = 0; k < pedido.produto.length; k++) {
+
+                const subtracao = pedido.produto[k].quantidade;
+
+                console.log('Valor para subtracao', subtracao)
+
+                for (let j = 0; j < pedidoFindOne.produto.length; j++) {
+
+                    if (pedido.produto[k].idProduto == pedidoFindOne.produto[j].idProduto) {
+
+                        console.log('Calculo de subtracao', pedidoFindOne.produto[j].quantidade - subtracao)
+
+                        const total = pedidoFindOne.produto[j].quantidade - subtracao;
+
+                        pedidoFindOne.produto[j].quantidade = total
+
+                        console.log('Subtracao Toral', total)
+                    }
+                }
+            }
+
+            const pedidoUpdate = await Pedido.findOneAndUpdate({ _id: pedidoId }, pedidoFindOne, { new: true })
+
+            if (pedidoUpdate == null) {
+                res.status(422).json({
+                    success: false,
+                    message: 'Pedido não pode ser atualizado!',
+                    data: [],
+                })
+            } else {
+                res.status(200).json({
+                    success: true,
+                    message: 'Atualização realizada com sucesso!',
+                    data: pedidoUpdate,
+                })
+            }
+        } else {
+
+            const pedidoFindOne = await Pedido.findOne({ _id: pedidoId })
+
+            for (let k = 0; k < pedido.produto.length; k++) {
+
+                let adicao = 0;
+
+                adicao = pedido.produto[k].quantidade
+
+                console.log('Valor para adição', adicao)
+
+                for (let j = 0; j < pedidoFindOne.produto.length; j++) {
+
+                    if (pedido.produto[k].idProduto == pedidoFindOne.produto[j].idProduto) {
+
+                        console.log('Calculo de Adição', pedidoFindOne.produto[j].quantidade - adicao)
+
+                        const total = pedidoFindOne.produto[j].quantidade + adicao;
+
+                        pedidoFindOne.produto[j].quantidade = total
+
+                        console.log('Soma das quantidades', total)
+                    }
+                }
+            }
+
+            const pedidoUpdate = await Pedido.findOneAndUpdate({ _id: pedidoId }, pedidoFindOne, { new: true })
+
+            if (pedidoUpdate == null) {
+                res.status(422).json({
+                    success: false,
+                    message: 'Pedido não pode ser atualizado!',
+                    data: [],
+                })
+            } else {
+                res.status(200).json({
+                    success: true,
+                    message: 'Atualização realizada com sucesso!',
+                    data: pedidoUpdate,
+                })
+            }
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: 'Não foi possível realizar a buscar do Pedido.',
+            error: error
+        })
+    }
 })
 
 module.exports = router
