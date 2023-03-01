30a2d7fc5535c7e133b8cb00c871d87517ba485c
GeorgeSouzaFreire <ge10l@hotmail.com>
2023-03-01 17:18:39 -0300

    Commit NTB-Delivery-Home-Office- 320

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const req = require('express/lib/request')
 const res = require('express/lib/response')
-const cors = require('cors')
+//const cors = require('cors')
 const { default: mongoose } = require('mongoose')
 const app = express()
   
-app.use(cors());
+//app.use(cors());
 // forma de ler JSON / middlewares
 app.use(express.json({ limit: '50mb' }))
 app.use(
