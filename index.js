84b84fe2239a960b5a5140fd91fd4279fd98b33c
GeorgeSouzaFreire <ge10l@hotmail.com>
2023-03-03 00:16:37 -0300

    Commit NTB-Delivery-Home-Office- 335

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const req = require('express/lib/request')
 const res = require('express/lib/response')
+
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
 const cors = require('cors')
 const { default: mongoose } = require('mongoose')
 const app = express()
@@ -20,6 +25,10 @@ app.use(
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.json())
+
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public-flutter')));
 
 //Rotas da API
 //const personRoutes = require('./routes/personRoutes')
