8678d5b4e550ee1a87028f7198506fc75ed48d2c
George Luiz Souza Freire <ge10l@hotmail.com>
2023-03-20 17:42:59 -0300

    Desktop-Office - 428

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get('/api', (req, res) => {
                 success: true,
                 message: "Acesso a API realizado com sucesso! Todos os direitos reservados",
                 data: {
-                   Version : "Version 0.4.0",
+                   Version : "Version 0.5.0",
                    Info: "Contact: application.gapp@gmail.com" 
                 },
             })
