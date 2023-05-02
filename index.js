b5fbd0746e6a1f99cf21e3cae794b52c206151f2
GeorgeSouzaFreire <ge10l@hotmail.com>
2023-05-02 13:22:37 -0300

    Commit NTB-Delivery-Home-Office- 441

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get('/api', (req, res) => {
                 success: true,
                 message: "Acesso a API realizado com sucesso! Todos os direitos reservados",
                 data: {
-                   Version : "Version 1.9.2",
+                   Version : "Version 1.9.3",
                    Info: "Contact: application.gapp@gmail.com" 
                 },
             })
