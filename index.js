fb39149300a573e65f3a0360497396d0771cc218
GeorgeSouzaFreire <ge10l@hotmail.com>
2023-07-03 17:24:00 -0300

    Commit - SSD-Work-Office-062

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.set('port', process.env.PORT || 3000);
 //app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public-flutter')));
-app.use(express.static(__dirname  + '/uploads'))
+app.use('/uploads', express.static('uploads'));
 //app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); 
 
 //Rotas da API
