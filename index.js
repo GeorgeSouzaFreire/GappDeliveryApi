a24b56d0c075930327d6eb9e059bd4772884e972
GeorgeSouzaFreire <ge10l@hotmail.com>
2023-07-03 17:01:19 -0300

    Commit - SSD-Work-Office-058

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.set('port', process.env.PORT || 3000);
 //app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public-flutter')));
-app.use(express.static(path.join(__dirname, 'uploads'))); 
+app.use(express.static(path.join(__dirname, '/uploads'))); 
 
 //Rotas da API
 //const personRoutes = require('./routes/personRoutes')
@@ -79,7 +79,7 @@ app.get('/api', (req, res) => {
                 success: true,
                 message: "Acesso a API realizado com sucesso! Todos os direitos reservados",
                 data: {
-                   Version : "Version 2.0.10",
+                   Version : "Version 3.0.0",
                    Info: "Contact: application.gapp@gmail.com" 
                 },
             })
