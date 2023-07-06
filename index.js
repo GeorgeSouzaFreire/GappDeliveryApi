4242d1882b17d6d246acf4ea446f4c2473750688
GeorgeSouzaFreire <ge10l@hotmail.com>
2023-07-06 13:06:13 -0300

    Commit - SSD-Work-Office-070

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/api', (req, res) => {
                 success: true,
                 message: "Acesso a API realizado com sucesso! Todos os direitos reservados",
                 data: {
-                   Version : "Version 2.0.10",
+                   Version : "Version 3.0.0",
                    Info: "Contact: application.gapp@gmail.com" 
                 },
             })
