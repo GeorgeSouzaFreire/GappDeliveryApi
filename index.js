e3e1c0e63ce3456eb29e5c9e0bc8f75b0282c460
Jun Kudo <jkudo@example.com>
2023-08-02 15:35:09 -0300

    Led-Office-034

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/api', (req, res) => {
                 success: true,
                 message: "Acesso a API realizado com sucesso! Todos os direitos reservados",
                 data: {
-                    Version: "Version 4.0.12",
+                    Version: "Version 5.0.0",
                     Info: "Contact: application.gapp@gmail.com",
                     Update: 'Usuário Por Id'
                 },
